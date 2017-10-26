061a84fbca6011dc862900f3a7767981689b0915
xuqiang <xuqiang@wafersystems.com>
2017-10-26 10:57:46 +0800

    add global spanning and intl

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { connect } from 'dva';
+import {connect} from 'dva';
 import {IntlProvider} from 'react-intl';
+import {LocaleProvider} from 'antd';
+import enUS from 'antd/lib/locale-provider/en_US';
+import {ENGLISH} from '../utils/constant';
+import {Spin} from 'antd'
 
-const App = ({children, i18n}) => {
+const App = ({children, i18n, loading}) => {
   return (
-    <IntlProvider locale={i18n.locale} messages={i18n.messages}>{children}</IntlProvider>
+    <Spin tip="loading..." spinning={loading.global} size="large">
+      <div style={{height: '100vh'}}>
+        <LocaleProvider locale={i18n.locale === ENGLISH ? enUS : null}>
+          <IntlProvider locale={i18n.locale} messages={i18n.messages}>{children}</IntlProvider>
+        </LocaleProvider>
+      </div>
+    </Spin>
   );
 };
 
